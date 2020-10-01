6a1850f86af4d1d5bb2dba4f4101fcdb211a6c1e
JKyukino <64891268+JKyukino@users.noreply.github.com>
2020-10-01 19:12:56 +0800

    添加了一些额外的功能

--- roll_game.py
+++ roll_game.py
@@ -1,17 +1,32 @@
 import random
-print('<<<<< GAME START! >>>>>')
-choice=input('Big or Small:')
-point1=random.randrange(1,7)
-point2=random.randrange(1,7)
-point3=random.randrange(1,7)
-x=point1+point2+point3
-print('<<<<< ROLE THE DICE!>>>>>')
-y=None
-if (x>=11 and x<=18) :
-	y='Big'
-else:
-	y='Small'
-if choice==y:
-	print('The points are [{},{},{}] You Win!'.format(point1,point2,point3))
-else:
-	print('The points are [{},{},{}] You Lose!'.format(point1,point2,point3))
+def roll():
+    point=[]
+    for i in range(1,4):
+        point.append(random.randrange(1,7))
+    point.append(point[0]+point[1]+point[2])
+    return point
+def game_result(x):
+    print('<<<<< ROLE THE DICE!>>>>>')
+    if (x >= 11 and x <= 18):
+       y = 'Big'
+    else:
+       y = 'Small'
+    return y
+coin=1000
+while coin > 0:
+    print('<<<<< GAME START! >>>>>')
+    choice = input('Big or Small:')
+    bet_coin = int(input('How much you wanna bet ? -'))
+    while coin-bet_coin<0:
+        print('error')
+        bet_coin = int(input('How much you wanna bet ? -'))
+    coin=coin-bet_coin
+    point=roll()
+    y=game_result(point[3])
+    if choice == y:
+       print('The points are [{},{},{}] You Win!'.format(point[0],point[1],point[2]))
+       coin = coin + 2 * bet_coin
+       print('You gained {},you have {} now'.format(bet_coin, coin))
+    else:
+       print('The points are [{},{},{}] You Lose!'.format(point[0],point[1],point[2]))
+       print('You lost {},you have {} now'.format(bet_coin, coin))
